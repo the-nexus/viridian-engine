a94b84b002a136d09fc3b00c2edb9587ecaf34fb
the-nexus <the-nexus@github.com>
2022-10-31 21:45:34 -0400

    CMake project setup

---
+++ GenerateSolution.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+SOLUTION_DIR="bin/"
+SOURCE_DIR="source/"
+
+# Create the solution directory if it doesn't exist then move to it
+if [ ! -d $SOLUTION_DIR ]; then
+    mkdir $SOLUTION_DIR
+fi
+
+cd $SOLUTION_DIR
+
+# Generate (or regenerate) the solution right here based on the CMakeList.txt in the source folder
+cmake ../$SOURCE_DIR
---
+++ source/core/Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World, I am Cerulean!" << std::endl;
+    return 0;
+}
