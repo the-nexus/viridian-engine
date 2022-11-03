3de39f47ea8807293bd2fcf7e298fe8088523de9
the-nexus <kevin.pulgarturcotte@gmail.com>
2022-11-03 09:45:16 -0400

    Changed folder layout to allow for future subdivisions of engine modules into libraries

--- GenerateSolution.sh
+++ GenerateSolution.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 BUILD_DIR="./bin/"
-CMAKE_LIST_DIR="./"
+CMAKE_LIST_DIR="./src/"
+
+# Update the Submodules to make sure they're downloaded before building
+sh "./scripts/UpdateSubmodules.sh"
 
 # Generate (or regenerate) the build solution based on the root CMakeList.txt
 cmake -S $CMAKE_LIST_DIR -B $BUILD_DIR -DGLFW_BUILD_DOCS=OFF
--- src/rendering/CeruleanRenderer.cpp
+++ src/rendering/CeruleanRenderer.cpp
@@ -1,7 +1,10 @@
 #include "CeruleanRenderer.h"
 
+#define GLFW_INCLUDE_GLU
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
 CeruleanRenderer::CeruleanRenderer()
 {
@@ -26,16 +29,61 @@ void CeruleanRenderer::CreateMainWindow(char const* title, int const width, int 
     if (!m_glWindow)
     {
         m_glWindow = glfwCreateWindow(width, height, title, nullptr, nullptr);
+        if (m_glWindow)
+        {
+            glfwMakeContextCurrent(m_glWindow);
+        }
     }
 }
 
 void CeruleanRenderer::Update()
 {
-    if (m_glWindow)
+    if (!m_glWindow)
     {
-        glfwSwapBuffers(m_glWindow);
-        glfwPollEvents();
+        return;
     }
+
+    // int width, height;
+    // glfwGetWindowSize(m_glWindow, &width, &height);
+    // height = height > 0 ? height : 1;
+
+    // glViewport(0, 0, width, height);
+
+    // glClearColor(0.f, 0.f, 0.f, 0.f);
+    // glClear(GL_COLOR_BUFFER_BIT);
+
+    // glMatrixMode(GL_PROJECTION);
+    // glLoadIdentity();
+    // gluPerspective(65.f, width / height, 0.001f, 1000.f);
+
+    // glMatrixMode(GL_MODELVIEW);
+    // glLoadIdentity();
+    // gluLookAt(
+    //     0.f, 1.f, 0.f,
+    //     0.f, 20.f, 0.f,
+    //     0.f, 0.f, 1.f
+    // );
+
+    // glPushMatrix();
+    // glTranslatef(0.f, 10.f, 0.f);
+
+    // glBegin(GL_TRIANGLES);
+    // {
+    //     glColor3f(1.f, 0.f, 0.f);
+    //     glVertex3f(-5.f, 0.f, -4.f);
+
+    //     glColor3f(0.f, 1.f, 0.f);
+    //     glVertex3f(5.f, 0.f, -4.f);
+
+    //     glColor3f(0.f, 0.f, 1.f);
+    //     glVertex3f(0.f, 0.f, 6.f);
+    // }
+    // glEnd();
+
+    // glPopMatrix();
+
+    glfwSwapBuffers(m_glWindow);
+    glfwPollEvents();
 }
 
 bool CeruleanRenderer::ShouldQuit() const
